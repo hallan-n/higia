fe7cb439f9042398149d1166f6dbd27a76c18184
Hállan Neves <104338375+hallan-n@users.noreply.github.com>
2022-06-11 20:57:56 -0300

    att

---
+++ click.js
@@ -0,0 +1,43 @@
+function clicou(){
+    document.getElementById('nave').style.visibility = "visible";
+}
+function clicous(){
+    document.getElementById('nave').style.visibility = "hidden";
+}
+
+
+function opc(){
+    document.getElementById('opc').style.visibility = "visible";
+    document.getElementById('list1').style.visibility = "visible";
+}
+function opca(){
+    document.getElementById('opca').style.visibility = "visible";
+    document.getElementById('list2').style.visibility = "visible";
+}
+function opcb(){
+    document.getElementById('opcb').style.visibility = "visible";
+    document.getElementById('list3').style.visibility = "visible";
+}
+function opcc(){
+    document.getElementById('opcc').style.visibility = "visible";
+    document.getElementById('list4').style.visibility = "visible";
+}
+function opcd(){
+    document.getElementById('opcd').style.visibility = "visible";
+    document.getElementById('list5').style.visibility = "visible";
+}
+
+function opc1(){
+    document.getElementById('opc').style.visibility = "hidden";
+    document.getElementById('opca').style.visibility = "hidden";
+    document.getElementById('opcb').style.visibility = "hidden";
+    document.getElementById('opcc').style.visibility = "hidden";
+    document.getElementById('opcd').style.visibility = "hidden";
+
+    document.getElementById('list1').style.visibility = "hidden";
+    document.getElementById('list2').style.visibility = "hidden";
+    document.getElementById('list3').style.visibility = "hidden";
+    document.getElementById('list4').style.visibility = "hidden";
+    document.getElementById('list5').style.visibility = "hidden";
+}
+
